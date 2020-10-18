d1f9d767408374fb8b2a586277f809f452dc27f0
Christoph Lipp <christoph@lipp.ws>
2020-10-18 13:24:45 +0200

    lets see

--- test/migration_source_test.exs
+++ test/migration_source_test.exs
@@ -13,9 +13,12 @@ defmodule MigrationSourceTest do
   test "test migration source has the correct name and retuns a value", %{config: config} do
     # construct a query
     migration_source = Config.get_migration_source(config)
+    schema = Keyword.get(config, :schema)
+
+    table = table_name(migration_source, schema)
 
     script =
-      "select major_version, minor_version, patch_version from #{migration_source}"
+      "select major_version, minor_version, patch_version from #{table}"
 
     # get the result from the database
     {:ok, result} = Database.execute_query(config, script)
@@ -26,4 +29,8 @@ defmodule MigrationSourceTest do
     assert minor >= 0
     assert patch >= 0
   end
+
+
+  defp table_name(migration_source, nil), do: migration_source
+  defp table_name(migration_source, schema), do: "#{schema}.#{migration_source}"
 end
